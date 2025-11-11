1acca01744abf2b7487c53e82359afe81221504b
WaleedEssam49 <essam.elsenawy@gmail.com>
2025-11-11 09:57:12 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -1,19 +1,20 @@
 #ifndef INSTRUCTOR_H
 #define INSTRUCTOR_H
 
+#include <iostream>
+#include <string>
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
 
-
-
-
-
-
-
-
-
-
-
+class Instructor : public Person {
+private:
+    string department;
+    int experienceYears;
+public:
+    Instructor(string n, int i, string d, int e);
+    void display();
+    string getDepartment();
+    int getExperience();
+};
 
 #endif
