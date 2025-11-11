f27c994697b553ddaeeb5ac5ad5213118c69d387
WaleedEssam49 <essam.elsenawy@gmail.com>
2025-11-11 09:56:44 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,21 +1,20 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-#include "Person.h"
+#include <iostream>
 #include <string>
-#using namespace std;
-//#write student class here
+#include "Person.h"
+using namespace std;
 
-
-
-
-
-
-
-
-
-
-
-
+class Student : public Person {
+private:
+    string major;
+    int yearLevel;
+public:
+    Student(string n, int i, string m, int y);
+    void display();
+    string getMajor();
+    int getYearLevel();
+};
 
 #endif
