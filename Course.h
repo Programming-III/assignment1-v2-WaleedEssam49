f58b257442d76fbcefccfd79ab0fc465b1ff84fa
WaleedEssam49 <essam.elsenawy@gmail.com>
2025-11-11 09:57:31 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,22 +1,24 @@
 #ifndef COURSE_H
 #define COURSE_H
 
+#include <iostream>
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
 
+class Course {
+private:
+    string courseName;
+    string courseCode;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+public:
+    Course(string name, string code, int max);
+    ~Course();
 
-
-
-
-
-
-
-
-
-
-
-
+    bool addStudent(Student s);
+    void displayCourseInfo();
+};
 
 #endif
